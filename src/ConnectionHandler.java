90be6a3e1b21d58b24b1e00fb0491e9fce7a9869
mjschuetze102 <mjs8474@rit.edu>
2019-02-21 08:35:33 -0500

    Added in a rough framework for what Server will look like

---
+++ src/ConnectionHandler.java
@@ -0,0 +1,69 @@
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Handles setting up a connection to the client
+ * Written by Michael Schuetze on 2/21/2019.
+ */
+public class ConnectionHandler {
+
+    /** Socket which all communication to the server will happen over **/
+    private ServerSocket serverSocket;
+
+    /** The endpoint which receives data from the inputManager **/
+    private Server server;
+
+    /**
+     * Initializes the server half of the connection
+     * @param server - the ConnectionEndpoint the input and output managers will communicate with
+     * @param serverSocket - the socket communication will happen over
+     */
+    public ConnectionHandler(Server server, ServerSocket serverSocket) {
+        this.server = server;
+        this.serverSocket = serverSocket;
+    }
+
+    /////////////////////////////////////////////////////
+    //              Class Functionality
+    /////////////////////////////////////////////////////
+
+    /**
+     * Runs the server allowing for users to connect
+     */
+    public void run() {
+        // Run the server establishing connections to all clients that attempt to connect
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+
+                // Create the input and output streams for the connection
+                ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
+                ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+
+                // Creates the Input and Output Managers for the client
+                new InputManager(server, inputStream);
+                // TODO: Create a class that stores the outputStreams
+            } catch (EOFException EOFex) {
+                System.out.println("[INFO] Server Connection Ended");
+            } catch (IOException IOex) {
+                System.out.println("[ERROR] Could Not Establish Connection");
+            }
+        }
+    }
+
+    /////////////////////////////////////////////////////
+    //              Getters and Setters
+    /////////////////////////////////////////////////////
+
+    /////////////////////////////////////////////////////
+    //               Helper Functions
+    /////////////////////////////////////////////////////
+
+    /////////////////////////////////////////////////////
+    //               Testing Purposes
+    /////////////////////////////////////////////////////
+}
---
+++ src/Server.java
@@ -0,0 +1,58 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.HashMap;
+
+/**
+ * Written by Michael Schuetze on 2/21/2019.
+ */
+public abstract class Server implements ConnectionEndpoint {
+
+    /** Manager that handles sending output **/
+    private OutputManager outputManager;
+
+    /**
+     * @param maxUsers - the maximum numbers of users who can use the server at once
+     * @param port - the port number the server is listening on
+     */
+    public Server(int maxUsers, int port) {
+        this.outputManager = new OutputManager();
+
+        try {
+            // Create a server socket
+            ServerSocket serverSocket = new ServerSocket(port, maxUsers);
+            new ConnectionHandler(this, serverSocket);
+        } catch (IOException IOex) {
+            System.out.println("[ERROR] Could Not Establish Server");
+        }
+    }
+
+    /////////////////////////////////////////////////////
+    //              Class Functionality
+    /////////////////////////////////////////////////////
+
+    /**
+     * Provide functionality for receiving messages over the client-server connection
+     * @param messageContents - contents for the Message to be sent over the client-server connection
+     */
+    public abstract void receiveMessage(HashMap<String, Object> messageContents);
+
+    /**
+     * Provide functionality for sending messages over the client-server connection
+     * @param messageContents - contents for the Message to be sent over the client-server connection
+     */
+    public void sendMessage(HashMap<String, Object> messageContents) {
+        // TODO Implement outputManager.sendMessage();
+    }
+
+    /////////////////////////////////////////////////////
+    //              Getters and Setters
+    /////////////////////////////////////////////////////
+
+    /////////////////////////////////////////////////////
+    //               Helper Functions
+    /////////////////////////////////////////////////////
+
+    /////////////////////////////////////////////////////
+    //               Testing Purposes
+    /////////////////////////////////////////////////////
+}
