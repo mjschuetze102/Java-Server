31ed34d562b51cda87255be087c9dd49ff8ff593
mjschuetze102 <mjs8474@rit.edu>
2019-02-25 01:31:51 -0500

    Added a bit more functionality to the server as an example

--- src/Sample/SampleClient.java
+++ src/Sample/SampleClient.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 /**
+ * Sample client for a chat room
  * Written by Michael Schuetze on 2/24/2019.
  */
 public class SampleClient extends Client {
@@ -18,17 +19,28 @@ public class SampleClient extends Client {
     //              Class Functionality
     /////////////////////////////////////////////////////
 
-    private void start() {
+    /**
+     * Starting code to initialize the client
+     * Sends a message which will be interpreted by the server
+     * @param name - name of the client
+     */
+    private void start(String name) {
         System.out.println("[INFO] Starting Client");
 
         HashMap<String, Object> messageContents = new HashMap<>();
         messageContents.put("message", "Client wants to connect");
+        messageContents.put("name", name);
         sendMessage(messageContents);
         System.out.println("[INFO] Sent Initialization Message");
 
         conversation();
     }
 
+    /**
+     * Provide functionality for receiving messages over the client-server connection
+     * @param messageContents - contents for the Message received over the client-server connection
+     * @param clientID - unique identifier for who sent the message
+     */
     @Override
     public void receiveMessage(HashMap<String, Object> messageContents, int clientID) {
         System.out.println(messageContents.get("message"));
@@ -42,6 +54,9 @@ public class SampleClient extends Client {
     //               Helper Functions
     /////////////////////////////////////////////////////
 
+    /**
+     * Loop which allows the user to communicate with everyone else on the server
+     */
     private void conversation() {
         while(true) {
             Scanner scanner = new Scanner(System.in);
@@ -59,6 +74,11 @@ public class SampleClient extends Client {
 
     public static void main(String[] args) {
         SampleClient client = new SampleClient("localhost", 9001);
-        client.start();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Name: ");
+        String name = scanner.nextLine();
+
+        client.start(name);
     }
 }
--- src/Sample/SampleServer.java
+++ src/Sample/SampleServer.java
@@ -2,12 +2,18 @@ package Sample;
 
 import ServerSide.Server;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
+ * Sample server for a chat room
  * Written by Michael Schuetze on 2/24/2019.
  */
 public class SampleServer extends Server {
+
+    /** Used to map a name to an ID and an ID to a name **/
+    private HashMap<String, Integer> nameToId = new HashMap<>();
+    private HashMap<Integer, String> idToName = new HashMap<>();
 
     private SampleServer(int maxQueuedUsers, int port) {
         super(maxQueuedUsers, port);
@@ -17,14 +23,40 @@ public class SampleServer extends Server {
     //              Class Functionality
     /////////////////////////////////////////////////////
 
+    /**
+     * Provide functionality for receiving messages over the client-server connection
+     * @param messageContents - contents for the Message received over the client-server connection
+     * @param clientID - unique identifier for who sent the message
+     */
     @Override
     public void receiveMessage(HashMap<String, Object> messageContents, int clientID) {
+        // Initialize the message to send
         HashMap<String, Object> newMessage = new HashMap<>();
 
-        newMessage.put("message", "[Client " + String.valueOf(clientID) + "] " + messageContents.get("message"));
+        // Get the message that was sent
+        String message = (String) messageContents.get("message");
 
-        //sendMessage(newMessage, new int[]{clientID});
-        sendMessageToAll(newMessage);
+        // See what actions to take based on message
+        if (message.equals("Client wants to connect")) {
+            // Map the newly connected client name to the unique identifier
+            mapClientName(clientID, (String) messageContents.get("name"));
+            newMessage.put("message", idToName.get(clientID) + " has connected");
+
+            sendMessageToAll(newMessage);
+        } else if (message.indexOf("/pm") == 0) {
+            // Private message a specific person on the server
+            String[] messagePieces = message.split(" ");
+            int sendTo = nameToId.get(messagePieces[1]);
+            newMessage.put("message", "{Private} [" + idToName.get(clientID) + "] " +
+                    String.join(" ", Arrays.copyOfRange(messagePieces, 2, messagePieces.length)));
+
+            sendMessage(newMessage, new int[]{clientID, sendTo});
+        } else {
+            // Send a normal message to the whole server
+            newMessage.put("message", "[" + idToName.get(clientID) + "] " + messageContents.get("message"));
+
+            sendMessageToAll(newMessage);
+        }
     }
 
     /////////////////////////////////////////////////////
@@ -35,11 +67,21 @@ public class SampleServer extends Server {
     //               Helper Functions
     /////////////////////////////////////////////////////
 
+    /**
+     * Maps the clientID to the client name so the server can keep track of people
+     * @param clientID - unique identifier for the client
+     * @param name - name for the client
+     */
+    private synchronized void mapClientName(int clientID, String name) {
+        nameToId.put(name, clientID);
+        idToName.put(clientID, name);
+    }
+
     /////////////////////////////////////////////////////
     //               Testing Purposes
     /////////////////////////////////////////////////////
 
     public static void main(String[] args) {
-        SampleServer server = new SampleServer(10, 9001);
+        new SampleServer(10, 9001);
     }
 }
